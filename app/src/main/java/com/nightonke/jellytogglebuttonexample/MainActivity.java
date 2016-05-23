0f4f5786e689b0c9e78343315345fab66d0e85ea
Weiping HUANG <Nightonke@outlook.com>
2016-05-23 08:51:36 +0800

    New apk.

--- app/src/main/java/com/nightonke/jellytogglebuttonexample/MainActivity.java
+++ app/src/main/java/com/nightonke/jellytogglebuttonexample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.nightonke.jellytogglebuttonexample;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -69,5 +73,51 @@ public class MainActivity extends AppCompatActivity
 
     private <T extends View> T findView(int id) {
         return (T) findViewById(id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_github:
+                goWeb("https://github.com/Nightonke/FaceOffToggleButton");
+                return true;
+            case R.id.action_developer:
+                goWeb("https://github.com/Nightonke");
+                return true;
+            case R.id.action_wowo:
+                goWeb("https://github.com/Nightonke/WoWoViewPager");
+                return true;
+            case R.id.action_boom:
+                goWeb("https://github.com/Nightonke/BoomMenu");
+                return true;
+            case R.id.action_cocoin:
+                goWeb("https://github.com/Nightonke/CoCoin");
+                return true;
+            case R.id.action_blur:
+                goWeb("https://github.com/Nightonke/BlurLockView");
+                return true;
+            case R.id.action_leeco:
+                goWeb("https://github.com/Nightonke/LeeCo");
+                return true;
+            case R.id.action_gw:
+                goWeb("https://github.com/Nightonke/GithubWidget");
+                return true;
+            case R.id.action_face:
+                goWeb("https://github.com/Nightonke/FaceOffToggleButton");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void goWeb(String url) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
     }
 }
